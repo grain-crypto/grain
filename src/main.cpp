ad8db3029cb4f8c7e3e591e4c3c7bbdb0aa17ba0
bee7 <bee7@github.com>
2014-05-26 12:12:41 +0400

    Add Google's LevelDB support
    This commit imports LevelDB database sources tree and implements alternative CTxDB storage methods implementation (backported from bitcoin 0.8).

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "alert.h"
 #include "checkpoints.h"
 #include "db.h"
+#include "txdb.h"
 #include "net.h"
 #include "init.h"
 #include "ui_interface.h"
@@ -75,6 +76,10 @@ const string strMessageMagic = "Grain Signed Message:\n";
 // Settings
 int64 nTransactionFee = MIN_TX_FEE;
 bool fStakeUsePooledKeys = false;
+
+// Used during database migration.
+bool fDisableSignatureChecking = false;
+
 
 //////////////////////////////////////////////////////////////////////////////
 //
@@ -2071,8 +2076,6 @@ bool CBlock::AddToBlockIndex(unsigned int nFile, unsigned int nBlockPos)
         if (!SetBestChain(txdb, pindexNew))
             return false;
 
-    txdb.Close();
-
     if (pindexNew == pindexBest)
     {
         // Notify UI to display prev block's coinbase if it was ours
@@ -2878,7 +2881,7 @@ void PrintBlockTree()
     }
 }
 
-bool LoadExternalBlockFile(FILE* fileIn)
+bool LoadExternalBlockFile(FILE* fileIn, ExternalBlockFileProgress *progress)
 {
     int64 nStart = GetTimeMillis();
 
@@ -2927,6 +2930,8 @@ bool LoadExternalBlockFile(FILE* fileIn)
                         nPos += 4 + nSize;
                     }
                 }
+                if (progress != NULL)
+                    (*progress)(4 + nSize);
             }
         }
         catch (std::exception &e) {
